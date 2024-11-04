b7c2447eef05ec89bf63a8b4b53ec34f2b680ce0
Yehor Heiko <yehorheiko.developer@gmail.com>
2024-11-04 13:44:41 +0200

    Check

--- Curent/src/App.tsx
+++ Curent/src/App.tsx
@@ -4,10 +4,9 @@ import CurrencyConvertor from "./components/currency-convertor";
 import "./App.css";
 
 function App() {
-  // https://api.frankfurter.app/latest?amount=1&from=USD&to=EUR
   return (
     <div className="min-h-screen bg-gray-300 flex flex-col items-center justify-center">
-      <div className="container">
+      <div className="container max-w-2xl mx-auto">
         <CurrencyConvertor />
       </div>
     </div>
--- Curent/src/components/currency-convertor.tsx
+++ Curent/src/components/currency-convertor.tsx
@@ -63,7 +63,7 @@ function CurrencyConvertor() {
   }, []);
 
   return (
-    <div className="max-w-xl my-10 p-5 bg-white rounded-lg shadow-md">
+    <div className="max-w-xl my-10 p-5 bg-white rounded-lg shadow-md ">
       <h2 className="mb-5 text-2xl font-semibold text-gray-700">
         Currency-Convertor
       </h2>
